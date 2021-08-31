24a43b1bb03e7e80ac362d61611002fac54f7a88
tennis-aa <andresalban94@gmail.com>
2021-08-30 22:43:15 -0400

    US Open dia 1

--- bracket3_update.py
+++ bracket3_update.py
@@ -12,7 +12,7 @@ b.loadFromFolder(path)
 
 # If there are changes to the draw before the tournament starts (e.g., withdrawals) the following commands may be useful
 # b.updatePlayers()
-# b.updateElo("Y Uchiyama (LL)",1446.0)
+# b.updateElo("Y Sugita",1526)
 # b.updateElobracket()
 
 b.updateResults(scrape=True)
