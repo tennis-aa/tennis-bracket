ad09f0d8d78bfd6cfb7a77d4224e661945d6f505
tennis-aa <andresalban94@gmail.com>
2021-10-06 17:52:07 -0400

    Indian Wells 2021 inicio

--- bracket1_create.py
+++ bracket1_create.py
@@ -1,12 +1,12 @@
 import pybracket
 
-tournament = "US Open 2021" 
-atplink = "https://www.atptour.com/en/scores/current/us-open/560/draws" # link to ATP website to scrape players
+tournament = "Indian Wells 2021" 
+atplink = "https://www.atptour.com/en/scores/current/indian-wells/404/draws" # link to ATP website to scrape players
 bracketSize = 128 # Number of players 128, 64, 32 (if there are byes in the tournament inlude them in the bracketSize, i.e., do not enter 48 or 24)
-path = None # By default the files will be saved to /docs, but you can choose another path
+path = None # By default the files will be saved to /docs/tournament, but you can choose another path
 surface = "hard" # all, hard, clay, or grass. This is used to obtain the Elo rating of the players. "all" is the default for overall Elo rating
 points_per_round = [1,2,3,5,7,10,15] # The number of points competitors receive for a correct pick in each round
-sets = 5 # 3 for best of three sets matches and 5 for best of five sets matches
+sets = 3 # 3 for best of three sets matches and 5 for best of five sets matches
 # You can control the appearance of the bracket by specifying the cellheight, vspace, hspace, and linewidth in pixels 
 
 pybracket.bracketRender(tournament, atplink, bracketSize, path, surface, points_per_round,sets)
